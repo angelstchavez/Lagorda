a2649fc88c7f5a057947a5f5f6345d00790f18fc
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:06:48 -0500

    Added the User class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public Role Role { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string SecondLastName { get; set; } = string.Empty;
+        public string IdentificationNumber { get; set; } = string.Empty;
+        public string IdentificationType { get; set; } = string.Empty;
+        public int BloodTypeId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Telephone { get; set; } = string.Empty;
+        public string Mobile { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
