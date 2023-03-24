90147c5b9692a78040b8a32038ae620d334070bf
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:12:18 -0500

    Added the Warranty class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/Warranty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class Warranty
+    {
+        public int Period { get; set; }
+        public WarrantyUnit Unit { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public enum WarrantyUnit
+    {
+        Day,
+        Month,
+        Year
+    }
+}
