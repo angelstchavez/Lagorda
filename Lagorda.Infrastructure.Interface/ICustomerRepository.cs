9b04d4886dfcc8571fd4e9b0044be5f8ced257e6
angelstchavez <angelstchavez@gmail.com>
2023-03-24 11:11:56 -0500

    Added the ICustomerRepository interface in to Infrastructure.Interface class library

---
+++ Lagorda.Infrastructure.Interface/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface ICustomerRepository
+    {
+        int Create(Customer customer, out string message);
+        bool Update(Customer customer, out string message);
+        bool Delete(Customer customer, out string message);
+        List<Customer> GetCustomers();
+    }
+}
