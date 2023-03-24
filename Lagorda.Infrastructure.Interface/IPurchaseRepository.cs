530fe979d1db607b31d64600bb75fad51e2aa9b2
angelstchavez <angelstchavez@gmail.com>
2023-03-24 11:56:11 -0500

    Added IPurchaseRepository interface

---
+++ Lagorda.Infrastructure.Interface/IPurchaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface IPurchaseRepository
+    {
+        bool Create(Purchase purchase, DataTable purchaseDetails, out string message);
+        int GetCorrelative();
+        List<Purchase> GetPurchases();
+    }
+}
