2be77a7990a379f69a2e502b61835126f72b8667
angelstchavez <angelstchavez@gmail.com>
2023-03-24 12:11:31 -0500

    Added IBrandRepository interface

---
+++ Lagorda.Infrastructure.Interface/IBrandRepository.cs
@@ -0,0 +1,13 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface IBrandRepository
+    {
+        int Create(Brand brand, out string message);
+        bool Update(Brand brand, out string message);
+        bool Delete(Brand brand, out string message);
+        List<Brand> GetBrands();
+    }
+}
