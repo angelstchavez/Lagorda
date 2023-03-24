ba2d9a38ee85f60705253ce4d13cbedbbaea83eb
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:08:52 -0500

    Added the Permission class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/Permission.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class Permission
+    {
+        public int PermissionId { get; set; }
+        public Role Role { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
