b3b370766865c6fcf90466ab531d7a7f36da0251
angelstchavez <angelstchavez@gmail.com>
2023-03-24 11:42:51 -0500

    Added the IPermissionRepository interface in to Infrastructure.Interface class library

---
+++ Lagorda.Infrastructure.Interface/IPermissionRepository.cs
@@ -0,0 +1,10 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface IPermissionRepository
+    {
+        List<Permission> GetPermissions(int userId);
+    }
+}
