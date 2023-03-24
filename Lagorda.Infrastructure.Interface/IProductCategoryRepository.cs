2ae5aa7c22d4749725ae465d1394dcd262020e53
angelstchavez <angelstchavez@gmail.com>
2023-03-24 11:45:26 -0500

    Added the IProductCategoryRepository interface in to Infrastructure.Interface class library

---
+++ Lagorda.Infrastructure.Interface/IProductCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface IProductCategoryRepository
+    {
+        int Create(ProductCategory productCategory, out string message);
+        bool Update(ProductCategory productCategory, out string message);
+        bool Delete(ProductCategory productCategory, out string message);
+        List<ProductCategory> GetProductCategories();
+    }
+}
