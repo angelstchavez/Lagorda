f67ff475f76ac37413696da57dcfcc5e238d54e4
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:13:09 -0500

    Added the ProductCategory class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class ProductCategory
+    {
+        public int ProductCategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
