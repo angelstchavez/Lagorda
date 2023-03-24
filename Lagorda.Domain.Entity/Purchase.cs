f2bc62e38bfedafb10b3950bd59ff60a068618c3
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:17:31 -0500

    Added the Purchase class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/Purchase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lagorda.Domain.Entity
+{
+    public class Purchase
+    {
+        public int PurchaseId { get; set; }
+        public User User { get; set; }
+        public Provider Provider { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentNumber { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<SaleDetail> SaleDetails { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
