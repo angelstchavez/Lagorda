5aea1dba096d1ba72c398ed6facbe839937919c3
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:11:23 -0500

    Added the Product class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Reference { get; set; }
+        public Brand Brand { get; set; }
+        public ProductSubCategory ProductSubCategory { get; set; }
+        public Warranty Warranty { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public int Stock { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
