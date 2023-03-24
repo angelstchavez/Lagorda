b2926a0362bcd84eca60cd384efb0302d6b6b53b
angelstchavez <angelstchavez@gmail.com>
2023-03-24 11:06:53 -0500

    Added the IRoleRepository interface in to Infrastructure.Interface class library

---
+++ Lagorda.Infrastructure.Interface/IRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface IRoleRepository
+    {
+        List<Role> GetRoles();
+    }
+}
