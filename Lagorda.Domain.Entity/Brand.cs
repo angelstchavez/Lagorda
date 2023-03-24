ae3e7fee1457333629763262f4e01b2c30db4994
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:10:21 -0500

    Added the Brand class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
