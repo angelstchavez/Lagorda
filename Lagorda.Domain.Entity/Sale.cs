2341ea87a1b95c9500a473be22668024a1346506
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:16:12 -0500

    Added the Sale class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/Sale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lagorda.Domain.Entity
+{
+    public class Sale
+    {
+        public int SaleId { get; set; }
+        public User User { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentNumber { get; set; }
+        public string CustomerDocument { get; set; }
+        public string CustomerName { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public decimal ChangeAmount { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<SaleDetail> SaleDetails { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
