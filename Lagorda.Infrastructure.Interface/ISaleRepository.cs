87306a6f65f0d5532217365f60e05212914a3ce9
angelstchavez <angelstchavez@gmail.com>
2023-03-24 11:20:50 -0500

    Added the ISaleRepository interface in to Infrastructure.Interface class library

---
+++ Lagorda.Infrastructure.Interface/ISaleRepository.cs
@@ -0,0 +1,17 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface ISaleRepository
+    {
+        bool AddStock(int productId, int quantity);
+        bool Create(Sale sale, DataTable saleDetails, out string message);
+        int GetCorrelative();
+        Sale GetSale(string number);
+        List<Sale> GetSales();
+        List<SaleDetail> GetSaleDetails(int saleId);
+        bool SubtractStock(int productId, int quantity);
+    }
+}
