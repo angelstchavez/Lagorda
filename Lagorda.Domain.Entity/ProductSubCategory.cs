6461a366e025cfe614a13fa8f9fcee76f41147e4
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:13:46 -0500

    Added the ProductSubCategory class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/ProductSubCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class ProductSubCategory
+    {
+        public int ProductSubCategoryId { get; set; }
+        public ProductCategory ProductCategory { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
