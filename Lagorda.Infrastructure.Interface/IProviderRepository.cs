f2d14a4800abb804bf3b65a6ff99a2231b12a75c
angelstchavez <angelstchavez@gmail.com>
2023-03-24 11:10:02 -0500

    Added the IProviderRepository interface in to Infrastructure.Interface class library

---
+++ Lagorda.Infrastructure.Interface/IProviderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface IProviderRepository
+    {
+        int Create(Provider provider, out string message);
+        bool Update(Provider provider, out string message);
+        bool Delete(Provider provider, out string message);
+        List<Provider> GetProviders();
+    }
+}
