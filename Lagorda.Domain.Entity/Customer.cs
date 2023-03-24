2ac201bfd5f0ff1087d6b64f68ab0a60ee586478
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:09:35 -0500

    Added the Customer class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string SecondLastName { get; set; } = string.Empty;
+        public string IdentificationNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Telephone { get; set; } = string.Empty;
+        public string Mobile { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
