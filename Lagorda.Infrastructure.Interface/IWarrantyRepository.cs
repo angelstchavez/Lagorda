d8c2bbd8db1b6b179ce70e76cd3e2aa5a83aa584
angelstchavez <angelstchavez@gmail.com>
2023-03-24 12:13:11 -0500

    Added the IWarrantyRepository interface

---
+++ Lagorda.Infrastructure.Interface/IWarrantyRepository.cs
@@ -0,0 +1,11 @@
+﻿using Lagorda.Domain.Entity;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface IWarrantyRepository
+    {
+        int Create(Warranty warranty, out string message);
+        bool Update(Warranty warranty, out string message);
+        bool Delete(Warranty warranty, out string message);
+    }
+}
