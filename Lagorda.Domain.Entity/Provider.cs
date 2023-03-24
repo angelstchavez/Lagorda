97061dc2c2a58c30f8c94a55bba445697fb6b5cb
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:14:32 -0500

    Added the Provider class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/Provider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class Provider
+    {
+        public int ProviderId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Telephone { get; set; } = string.Empty;
+        public string Mobile { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string WebPage { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
