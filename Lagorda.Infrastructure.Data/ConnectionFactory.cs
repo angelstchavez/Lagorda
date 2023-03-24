cec9df6284f1b7b1af99ffcf4b3e7b2a8f664e0a
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:50:42 -0500

    Added the ConnectionFactory class in the Infrastructure.Data class library

---
+++ Lagorda.Infrastructure.Data/ConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+
+namespace Lagorda.Infrastructure.Data
+{
+    public class ConnectionFactory
+    {
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+    }
+}
