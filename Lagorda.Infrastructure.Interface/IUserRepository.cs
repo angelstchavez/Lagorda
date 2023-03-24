fc797a1c0d48db20c97a04da41ecc7f327c476da
angelstchavez <angelstchavez@gmail.com>
2023-03-24 11:03:49 -0500

    Added the IUserRepository interface in to Infrastructure.Interface class library

---
+++ Lagorda.Infrastructure.Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface IUserRepository
+    {
+        int Create(User user, out string message);
+        bool Update(User user, out string message);
+        bool Delete(User user, out string message);
+        List<User> GetUsers();
+        List<User> ValidateUsers();
+    }
+}
