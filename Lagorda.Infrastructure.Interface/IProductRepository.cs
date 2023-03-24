5a72564f72823aa1e2e71000a49479277c9ed514
angelstchavez <angelstchavez@gmail.com>
2023-03-24 11:38:49 -0500

    Added the IProductRepository interface in to Infrastructure.Interface class library

---
+++ Lagorda.Infrastructure.Interface/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Lagorda.Domain.Entity;
+using System.Collections.Generic;
+
+namespace Lagorda.Infrastructure.Interface
+{
+    public interface IProductRepository
+    {
+        int Create(Product product, out string message);
+        bool Update(Product product, out string message);
+        bool Delete(Product product, out string message);
+        List<Product> GetProducts();
+    }
+}
