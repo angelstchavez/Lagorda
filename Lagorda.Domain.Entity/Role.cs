362b8d88423a9bd4e91c29f07687abb6d307ba31
angelstchavez <angelstchavez@gmail.com>
2023-03-24 10:07:49 -0500

    Added the Role class to the Domain.Entity class library

---
+++ Lagorda.Domain.Entity/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lagorda.Domain.Entity
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
